0afdd0b539b932933947c202b82363c27e706470
matisse babonneau <matisse.babonneau@orange.com>
2023-02-03 09:54:11 +0100

    1675414444118354

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+let menuIsOpened = false;
+
+const toggle = document.querySelector(".menu-toggle");
+const toggleOpen = toggle.querySelector(".open");
+const toggleClose = toggle.querySelector(".close");
+const menuItem = document.querySelector(".menu-item");
+
+toggleMenu();
+
+toggle.addEventListener("click", () => {
+  menuIsOpened = !menuIsOpened;
+  toggleMenu();
+});
+
+function toggleMenu() {
+  if (!menuIsOpened) {
+    toggleOpen.style.display = "block";
+    toggleClose.style.display = "none";
+    menuItem.classList.remove("open");
+  } else {
+    toggleOpen.style.display = "none";
+    toggleClose.style.display = "block";
+    menuItem.classList.add("open");
+  }
+}
