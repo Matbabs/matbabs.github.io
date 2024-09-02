8d366261c48ecf3cad993ef7ee56908742eb5ef8
BABONNEAU Matisse <dnbh7461@EB-L-OR1031401.ad-subs.w2k.francetelecom.fr>
2024-09-02 11:54:56 +0200

    mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,32 @@
 let navBar = document.getElementById("navBar");
+let menu = document.getElementById("hamburger");
+let menuIsOpen = false;
 
-window.addEventListener("scroll", (event) => {
+window.addEventListener("scroll", () => {
   navBar.style.opacity = this.scrollY > 50 ? "100" : "0";
   navBar.style.pointerEvents = this.scrollY > 50 ? "all" : "none";
+});
+
+menu.addEventListener("click", () => {
+  menuIsOpen = !menuIsOpen;
+  if (menuIsOpen) {
+    menu.classList.add("fa-xmark");
+    menu.classList.remove("fa-bars");
+    navBar.classList.add("mobile");
+  } else {
+    menu.classList.add("fa-bars");
+    menu.classList.remove("fa-xmark");
+    navBar.classList.remove("mobile");
+  }
+});
+
+navBar.addEventListener("click", (event) => {
+  if (
+    event.target.id != "hamburger" &&
+    ["SPAN", "I", "A"].indexOf(String(event.target.nodeName)) != -1
+  ) {
+    menu.click();
+  }
 });
 
 function returnHome() {
